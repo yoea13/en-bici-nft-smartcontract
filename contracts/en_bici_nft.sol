e85346e0f0b50eb44d0233dac720e6bc375d9631
yoea13 <yoea13@gmail.com>
2023-03-20 17:33:45 -0300

    avance menor

--- contracts/en_bici_nft.sol
+++ contracts/en_bici_nft.sol
@@ -5,19 +5,30 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+error en_bici_nft__onlyOneNFT();
 
 contract en_bici_nft is ERC721, ERC721URIStorage, Ownable {
+    /* State Variables */
     using Counters for Counters.Counter;
-
+    mapping(address => bool) public owners;
     Counters.Counter private _tokenIdCounter;
+
+    /* Events */
+    event mintedNFT(address indexed member);
 
     constructor() ERC721("En bici", "ENBICI") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
+        if (owners[to]) {
+            revert en_bici_nft__onlyOneNFT();
+        }
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        owners[to] = true;
+        emit mintedNFT(to);
     }
 
     // The following functions are overrides required by Solidity.
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
     goerli: {
       url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
